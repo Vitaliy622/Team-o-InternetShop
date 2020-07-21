f234fdd236ffd8183bd148436fb0b0a7f11a7554
Oksana Miazina <o.miazina@tickets.com.ua>
2020-07-21 11:04:10 +0300

    IS-1000 test

--- src/main/java/view/impl/AdminMainMenu.java
+++ src/main/java/view/impl/AdminMainMenu.java
@@ -4,8 +4,6 @@ package view.impl;
 import view.Menu;
 
 public class AdminMainMenu implements Menu {
-    private String[] items = {"1.UsersMenu", "2.OrdersMenu", "3.ProductsMenu"};
-
     @Override
     public void show() {
         System.out.println("");
